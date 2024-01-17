70e297ad94f0b3a7026e08250e11952cca4e373c
Erica de Bem <ericadebemm@gmail.com>
2024-01-17 15:44:30 +0100

    att function

--- controller/bussController.js
+++ controller/bussController.js
@@ -1,20 +1,21 @@
 import { Buss } from "../model/bussModel.js";
 
-const handleError = (error) => {
+const handleError = (error, res) => {
   console.error(error.msg);
   return res.status(500).json(error);
 };
-const handleTrue = (true) => {
-  buss ? res.status(201).json({ buss })
-  : res.status(404).json({ msg: "Buss not found" })
+const handleTrue = (buss, res) => {
+  return buss
+    ? res.status(201).json({ buss })
+    : res.status(404).json({ msg: "Buss not found" });
 };
 
 export const getBuss = async (req, res) => {
   try {
     const buss = await Buss.findById(req.params.id);
-    buss {handleTrue(true)}
+    handleTrue(buss, res);
   } catch (error) {
-    handleError(error)
+    handleError(error, res);
   }
 };
 export const getAllBuss = async (req, res) => {
@@ -22,16 +23,15 @@ export const getAllBuss = async (req, res) => {
     const buss = await Buss.find({});
     res.status(201).json({ buss });
   } catch (error) {
-    handleError(error);
+    handleError(error, res);
   }
 };
 export const createBuss = async (req, res) => {
   try {
     const buss = await Buss.create(req.body);
-    console.log(req.body);
-    res.status(201).json({ buss });
+    handleTrue(buss, res);
   } catch (error) {
-    handleError(error);
+    handleError(error, res);
   }
 };
 export const updateBuss = async (req, res) => {
@@ -39,11 +39,9 @@ export const updateBuss = async (req, res) => {
     const buss = await Buss.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
-    buss
-      ? res.status(201).json({ buss })
-      : res.status(404).json({ msg: "Buss not found" });
+    handleTrue(buss, res);
   } catch (error) {
-    handleError(error);
+    handleError(error, res);
   }
 };
 export const deleteBuss = async (req, res) => {
@@ -51,6 +49,6 @@ export const deleteBuss = async (req, res) => {
     const buss = await Buss.findByIdAndDelete(req.params.id);
     res.status(201).json({ buss });
   } catch (error) {
-    handleError(error);
+    handleError(error, res);
   }
 };
