0fd794811790d8cbd9aef01cfcfa82709495c2b6
Erica de Bem <ericadebemm@gmail.com>
2024-01-17 11:15:03 +0100

    T

--- employeeModel.js
+++ employeeModel.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-export const employeeSchema = new mongoose.Schema({
+export const employeeSchema = new mongoose.Schema(
+  {
     name: String,
     sector: String,
-},
-{
+  },
+  {
     collection: "employee",
     versionKey: false,
     timestamps: true,
-}
+  }
 );
 
 export const Employee = mongoose.model("Employee", employeeSchema);
