6f41a4874d6221aa18c8c7a8bf7a59f6a73a3900
Erica de Bem <ericadebemm@gmail.com>
2024-01-18 09:29:15 +0100

    att buss

--- model/bussModel.js
+++ model/bussModel.js
@@ -1,8 +1,15 @@
 import express from "express";
 export const bussSchema = new mongoose.Schema(
     {
-      
-      
+      bussNumber : {
+        type: Number,
+        require: true
+      },
+      capacity: {
+        type: Number,
+        require: true
+      },
+      driver: true
     },
     {
       collection: "buss",
